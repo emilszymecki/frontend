d30d22fc07cadb555dac618e406e11c160e2c590
Emil <emilszymecki@gmail.com>
2018-10-24 13:50:40 +0200

    wt

--- components/Items.js
+++ components/Items.js
@@ -1,12 +1,14 @@
-import React, {Component} from 'react'
-import {Query} from 'react-apollo'
-import gql from 'graphql-tag'
-import styled from 'styled-components'
-import Item from './Item'
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Item from './Item';
+import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -18,7 +20,7 @@ const ALL_ITEMS_QUERY = gql`
 `;
 
 const Center = styled.div`
-	text-align:center;
+  text-align: center;
 `;
 
 const ItemsList = styled.div`
@@ -29,27 +31,31 @@ const ItemsList = styled.div`
   margin: 0 auto;
 `;
 
-class Items extends Component{
-	render(){
-		return(
-				<div>
-					<p>Items</p>
-					<Center>
-						<Query query={ALL_ITEMS_QUERY}>
-						 {({ data, error, loading }) => {
-				            if (loading) return <p>Loading...</p>;
-				            if (error) return <p>Error: {error.message}</p>;
-				            return (
-				               <ItemsList>{data.items.map(item => <Item item={item} key={item.id} />)}</ItemsList>
-				            );
-				         }}
-						</Query>	
-					</Center>
-					
-				</div>
-			)
-	}
+class Items extends Component {
+  render() {
+    return (
+      <Center>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          // fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage,
+          }}
+        >
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemsList>{data.items.map(item => <Item item={item} key={item.id} />)}</ItemsList>
+            );
+          }}
+        </Query>
+        <Pagination page={this.props.page} />
+      </Center>
+    );
+  }
 }
 
-export default Items
-export {ALL_ITEMS_QUERY}
+export default Items;
+export { ALL_ITEMS_QUERY };
